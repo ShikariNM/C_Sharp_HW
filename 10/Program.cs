3466f941fee7a156bb6016d8c37d60d7066dfdcd
Nikita Mavrinsky <sharabarovnv@gmail.com>
2023-01-31 12:53:23 +0600

    done task 10

---
+++ 10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и
+// на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8 
+// 918 -> 1
+
+Console.Clear ();
+Console.Write ("Enter the number: ");
+int num = int.Parse (Console.ReadLine ());
+Console.WriteLine($"{(num % 100 - num % 10) / 10}");
